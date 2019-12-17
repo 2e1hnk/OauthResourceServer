d890c64545a80685bf792c268d60b08a68072091
2E1HNK <2e1hnk@deadbeef.cafe>
2019-12-17 11:19:10 +0000

    Initial Commit

---
+++ OauthResourceServer/src/main/java/cafe/deadbeef/resource_server/ResourceServerWebConfig.java
@@ -0,0 +1,13 @@
+package cafe.deadbeef.resource_server;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "cafe.deadbeef.TestAuthServer.web.controller" })
+public class ResourceServerWebConfig implements WebMvcConfigurer {
+    //
+}
